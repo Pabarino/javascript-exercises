a144599009a7897ceabebfbbb36c9fc061f976a7
Pabarino <55687027+Pabarino@users.noreply.github.com>
2024-06-08 09:23:52 +0200

    Exercises 5, 6 and 7 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let sum = 0;
+    let start = 0;
+    let end = 0;
 
+    if (numOne<0||numTwo<0||(Number.isInteger(numOne)===false)||(Number.isInteger(numTwo)===false)) {
+        return ("ERROR");
+    }
+    if (numOne>numTwo) {
+        end = numOne;
+        start = numTwo; 
+    }
+    else {
+        end = numTwo;
+        start = numOne;  
+    }
+
+    for (let i=start; i<=end;i++){
+        sum = sum+i;
+    }
+
+    
+    return sum;
 };
+
+sumAll(1, "All");
 
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fa) {
+  return parseFloat((((fa-32)*5)/9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(ce) {
+  return parseFloat((((ce*9)/5)+32).toFixed(1));
 };
+
+console.log(convertToCelsius(100));
 
 // Do not edit below this line
 module.exports = {
