d3946b1113741aff191a2179bd456cfe44b393db
Pabarino <55687027+Pabarino@users.noreply.github.com>
2024-06-13 22:59:01 +0200

    Did some exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,30 @@
-const fibonacci = function() {
+//We create an array of numbers following the fibonacci sequence with a length that is passed to the function
 
+const fibonacci = function(number) {
+    
+    let counter;
+
+    if (number !== 'number') {
+        counter = parseInt(number);
+    }
+    else {
+        counter = number;
+    }
+
+    if (counter === 0) {return 0};
+
+    if (counter < 0) { return 'OOPS'};
+    
+    let firstNum = 1;
+    let secondNum = 0;
+
+    for (let i = 2; i <= counter; i++){
+        let currentNum = firstNum + secondNum;
+        secondNum = firstNum;
+        firstNum = currentNum;
+    };
+
+    return firstNum;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,6 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const oldestPerson = array.sort((a,b) => (a.yearOfDeath-a.yearOfBirth)>(b.yearOfDeath-b.yearOfBirth) ? -1 : 1);
+    return oldestPerson[0];
 };
 
 // Do not edit below this line
